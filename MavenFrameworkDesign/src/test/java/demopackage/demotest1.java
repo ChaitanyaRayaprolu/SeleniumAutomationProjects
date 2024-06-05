e6b8137483586b81b53cb2e1a61db978c7759e60
chaitanya <rayaproluchaitanya@gmail.com>
2024-06-05 08:22:24 +0530

    New package created

---
+++ MavenFrameworkDesign/src/test/java/demopackage/demotest1.java
@@ -0,0 +1,12 @@
+package demopackage;
+
+import org.testng.annotations.Test;
+
+public class demotest1 {
+	
+	@Test
+	public void test1() {
+		System.out.println("This is demotest 1");
+	}
+
+}
---
+++ MavenFrameworkDesign/src/test/java/demopackage/demotest2.java
@@ -0,0 +1,12 @@
+package demopackage;
+
+import org.testng.annotations.Test;
+
+public class demotest2 {
+	
+	@Test
+	public void test1() {
+		System.out.println("This is demo test2");
+	}
+
+}
