9e027bd954c33a60051ea1da9a9abaa14d4f905e
chaitanya <rayaproluchaitanya@gmail.com>
2024-06-06 23:07:25 +0530

    Commit for develop branch

--- MavenFrameworkDesign/src/test/java/demopackage/demotest2.java
+++ MavenFrameworkDesign/src/test/java/demopackage/demotest2.java
@@ -8,5 +8,15 @@ public class demotest2 {
 	public void test1() {
 		System.out.println("This is demo test2");
 	}
+	
+	@Test
+	public void test2() {
+		System.out.println("This is demo test2");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("This is demo test2");
+	}
 
 }
