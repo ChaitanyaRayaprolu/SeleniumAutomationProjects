f378cefc5993b0b40a11eb6e6625baedea631a4b
chaitanya <rayaproluchaitanya@gmail.com>
2024-09-22 23:52:15 +0530

    commit - 22/09/2024

--- PlayWrightAutomation/tests/ClientApp.spec.js
+++ PlayWrightAutomation/tests/ClientApp.spec.js
@@ -2,13 +2,15 @@ const {test, expect} = require('@playwright/test');
 
 test ('Client App Login', async ({page})=>
     {
+         const email = "rahultest24@gmail.com";
+         const password = "Test@12345";
          const products = page.locator(".card-body")         
          const productName = 'ADIDAS ORIGINAL';
 
 
          await page.goto("https://rahulshettyacademy.com/client/");
-         await page.locator("#userEmail").fill("rahultest24@gmail.com");
-         await page.locator("#userPassword").fill("Test@12345");
+         await page.locator("#userEmail").fill(email);
+         await page.locator("#userPassword").fill(password);
          await page.locator("#login").click();
          await page.waitForLoadState("networkidle");  //kind of wait
          //await page.locator(".card-body b").first().waitFor();
@@ -40,5 +42,32 @@ test ('Client App Login', async ({page})=>
                }
          }
 
-         await page.pause();
+         await page.locator(".field input").nth(1).fill("123");
+         await page.locator(".field input").nth(2).fill("Rahul Shetty");
+         await page.locator(".field input").nth(3).fill("rahulshettyacademy");
+         await page.locator(".btn.btn.btn-primary.mt-1").click();
+         await page.waitForLoadState("networkidle");
+         await page.locator("p:has-text('* Coupon Applied')").waitFor();
+         const bln1 = await page.locator("p:has-text('* Coupon Applied')").isVisible();
+         expect(bln1).toBeTruthy();
+         expect(page.locator(".user__name [type='text']").first()).toHaveText(email);         
+         await page.locator(".action__submit").click();
+         await expect (page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+         const orderId =  await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+     //     const  trimOrderId = orderId.match(/\| (\w+) \|/)[1];
+         await page.locator("button[routerlink*='/dashboard/myorders']").click();
+         await page.locator("tbody").waitFor();
+         const rows = await page.locator("tbody tr");
+         for(let i=0; i<await rows.count(); ++i)
+          {
+               const rowOrderId = await rows.nth(i).locator("th").textContent();
+               if(orderId.includes(rowOrderId)){
+                    await rows.nth(i).locator("button").first().click();
+                    break;
+               }
+         }
+         const orderIdDetails= await page.locator(".col-text.-main").textContent();
+         expect (orderId.includes(orderIdDetails)).toBeTruthy();
+         
+
     }); 
